01c1eb6eb1701af2f241d62f4f49c2049e75763e
tatsuya1173 <55310693+tatsuya1173@users.noreply.github.com>
2020-12-15 17:56:29 +0900

    3

--- html/common/config.php
+++ html/common/config.php
@@ -4,5 +4,6 @@
 //$db = new mysqli ('localhost', 'root', 'root', 'greatappdb');
 //docker
 $db = new mysqli('mysql', 'root', 'pass', 'greatappdb');
+//$mysqli = new mysqli('Host or IP', 'User', 'Password', 'DBName');
 session_start();
 ?>
